05db8ccb5ef4b26183e5ec66653b4f0cd781b969
developerfahad1 <fahadgraphic.pk>
2024-02-21 23:43:32 -0800

    app.js and index.html file created

---
+++ App.js
@@ -0,0 +1,19 @@
+// alert("HELLO MUHAMMAD FAHAD"); 
+
+alert("HI JAVASCRIPT ");
+
+
+// console.log("Hello Muhammad Fahad");
+
+// console.log("hello world");
+
+console.log("hi fahad");
+
+
+// THERE ARE FOR DATATYPES OF JAVASCRIPT
+
+// string  fakfkkfklsflkj
+// number 1251535
+// boolean true / false 
+
+object 
